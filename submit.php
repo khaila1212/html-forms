4f38acb1f95a551d47672ed16f11de10082d9bc2
pinakamasarapmoira <andriettemoira.pastoral@sdca.edu.ph>
2024-09-14 19:07:53 +0800

    khai

---
+++ submit.php
@@ -0,0 +1,18 @@
+<?php
+
+$first_name = $_POST["fullname"];
+$first_last = $_POST["first_last"];
+$email = $_POST["email"];
+$password = $_POST["password"];
+$date = $_POST["date"];
+
+
+echo "first name:". $first_name. "<br>";
+echo "last name:". $first_last. "<br>";
+echo "email:". $email. "<br>";
+echo "password:". $password. "<br>";
+echo "date:". $date. "<br>";
+
+
+?>
+
